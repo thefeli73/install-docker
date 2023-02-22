1e9e20a07171d18afaba36a41a95c0cdaac76d5d
Felix Schulze <39433012+thefeli73@users.noreply.github.com>
2023-02-22 09:54:38 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd /root
+
+#updates
+apt update && apt upgrade -y
+apt install -y unattended-upgrades apt-config-auto-update
+
+#docker
+apt install -y curl
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh ./get-docker.sh
+touch docker-compose.yml
+
+#update script
+cat >> update.sh << 'END'
+#!/bin/bash
+
+cd /root
+docker compose pull
+docker compose up --detach --force-recreate --pull always
+docker image prune -f
+END
+
+chmod +x update.sh
+ln -s /root/update.sh /etc/cron.weekly/update_docker
