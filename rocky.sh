9576e2907d2073b91f4c7a9a2552065f5f81e2c8
Felix Schulze <39433012+thefeli73@users.noreply.github.com>
2023-02-23 13:45:05 +0100

    Update rocky.sh

--- rocky.sh
+++ rocky.sh
@@ -33,7 +33,7 @@ yum install -y yum-utils
 yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl start docker
 systemctl enable docker
 
